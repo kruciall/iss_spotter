44c633f5f848ef29e61453a5936c61dfa7126695
Vinson Wong <vinson.wong93@gmail.com>
2022-09-29 19:42:35 -0400

    implemented all 4 parts of ISS activity

---
+++ index.js
@@ -0,0 +1,47 @@
+const { nextISSTimesForMyLocation } = require('./iss');
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  printPassTimes(passTimes);
+});
+
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:' , ip);
+// })
+
+// fetchCoordsByIP('142.198.2.2', (error, ip) => {
+//   if (error) {
+//     console.log("It didn't work" , error);
+//     return;
+//   }
+//   console.log('It worked! Longitude latitude are:', ip);
+// });
+
+// const exampleCoords = { latitude: '37.3860517', longitude: '-122.08385' };
+
+// fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned flyover times:' , passTimes);
+// });
+
